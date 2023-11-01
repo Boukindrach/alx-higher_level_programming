42da4d1f532ef50586460ca0af7ab988df1cc5b6
Boukind Rachid <boukind.r1@gmail.com>
2023-10-31 17:40:40 -0700

    z to A

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i)), end="")
+    if i % 2 == 0:
+        print("{}".format(chr(i)), end="")
+    else:
+        print('{}'.format(chr(i).upper()), end="")
