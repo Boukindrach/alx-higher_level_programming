f7f2a734fafd6d065dd07f27ad3f092b372266d4
Boukind Rachid <boukind.r1@gmail.com>
2023-11-02 03:32:39 -0700

    Update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-from calculator_1 import add, div, sub, mul
-a = 10
-b = 5
-x = add(a, b)
-c = "{}".format(x)
-print(f"{a} + {b} = {c}")
+if __name__ == "__main__":
+    from calculator_1 import add, div, sub, mul
+    a = 10
+    b = 5
+    x = add(a, b)
+    c = "{}".format(x)
+    print(f"{a} + {b} = {c}")
 
-x = sub(a, b)
-c = "{}".format(x)
-print(f"{a} - {b} = {c}")
+    x = sub(a, b)
+    c = "{}".format(x)
+    print(f"{a} - {b} = {c}")
 
-x = mul(a, b)
-c = "{}".format(x)
-print(f"{a} * {b} = {c}")
+    x = mul(a, b)
+    c = "{}".format(x)
+    print(f"{a} * {b} = {c}")
 
-x = div(a, b)
-c = "{}".format(x)
-print(f"{a} / {b} = {c}")
+    x = div(a, b)
+    c = "{}".format(x)
+    print(f"{a} / {b} = {c}")
