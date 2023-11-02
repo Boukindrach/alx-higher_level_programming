f1d799cdc0df741fa53ffb8c02967c1ee6f687c6
Boukind Rachid <boukind.r1@gmail.com>
2023-11-02 13:09:37 -0700

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,32 +2,32 @@
 
 if __name__ == "__main__":
 
-from calculator_1 import add, mul, div, sub
-import sys
+    from calculator_1 import add, mul, div, sub
+    import sys
 
-x = len(sys.argv) - 1
-a = int(sys.argv[1])
-b = int(sys.argv[x])
-operator = sys.argv[x - 1]
-operators = {"+", "*", "-", "/"}
+    x = len(sys.argv) - 1
+    a = int(sys.argv[1])
+    b = int(sys.argv[x])
+    operator = sys.argv[x - 1]
+    operators = {"+", "*", "-", "/"}
 
-if x < 3:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-
-else:
-    if operator in operators:
-        if operator == '+':
-            print(add(a, b))
-
-        elif operator == '*':
-            print(mul(a, b))
-
-        elif operator == '-':
-            print(sub(a, b))
-
-        elif operator == '/':
-            print(div(a, b))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if x < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
+    else:
+        if operator in operators:
+            if operator == '+':
+                print(add(a, b))
+
+            elif operator == '*':
+                print(mul(a, b))
+
+            elif operator == '-':
+                print(sub(a, b))
+
+            elif operator == '/':
+                print(div(a, b))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
