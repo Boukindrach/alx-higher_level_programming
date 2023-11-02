73f461f8535de3ced918231e55bb40a248d85d28
Boukind Rachid <boukind.r1@gmail.com>
2023-11-02 05:06:28 -0700

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     x = len(sys.argv) - 1
     if x == 0:
         print(f"{x} arguments.")
+    elif x == 1:
+        print(f"{x} argument:")
+        print(f"{x}: {sys.argv[x]}")
     else:
         print(f"{x} arguments:")
         for a in range(1, x + 1):
