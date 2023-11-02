2205fb2ff0d1c0265b3fc2542665c08d9ba6b45c
Boukind Rachid <boukind.r1@gmail.com>
2023-11-02 02:41:27 -0700

    calculation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from add_0 import add
-c = ""
 a = 1
 b = 2
 c = "{}".format(add(a, b))
---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from calculator_1 import add, div, sub, mul
+a = 10
+b = 5
+x = add(a, b)
+c = "{}".format(x)
+print(f"{a} + {b} = {c}")
+
+x = sub(a, b)
+c = "{}".format(x)
+print(f"{a} - {b} = {c}")
+
+x = mul(a, b)
+c = "{}".format(x)
+print(f"{a} * {b} = {c}")
+
+x = div(a, b)
+c = "{}".format(x)
+print(f"{a} / {b} = {c}")
