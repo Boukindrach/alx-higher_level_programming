c605c1280add85974887d4856d920d6206c7fbee
Boukind Rachid <boukind.r1@gmail.com>
2023-11-03 14:57:28 -0700

    copy list to a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    x = len(my_list)
+    if x < 0:
+        return my_list
+    elif x < idx:
+        return my_list
+    else:
+        my_new_list = list(my_list)
+        my_new_list[idx] = element
+        return my_new_list
+    return my_list
+
+
+if __name__ == "__main__":
+    new_in_list()
