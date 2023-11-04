15fc4d93bd90658b60046b32899c9d5c7d3e2133
Boukind Rachid <boukind.r1@gmail.com>
2023-11-04 06:53:56 -0700

    fixing task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     x = len(my_list)
-    if x < idx:
+    if x <= idx:
         return my_list
     elif idx < 0:
         return my_list
