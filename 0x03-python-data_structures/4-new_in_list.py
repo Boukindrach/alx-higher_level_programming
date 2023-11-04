17dce229867ae835c96dd6a89410977dc2524bc9
Boukind Rachid <boukind.r1@gmail.com>
2023-11-04 07:24:27 -0700

    fixing task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,12 +3,13 @@ def new_in_list(my_list, idx, element):
     x = len(my_list)
     if x < 0:
         return my_list
-    elif x <= idx:
+
+    if x <= idx:
         return my_list
-    else:
-        my_new_list = my_list[:]
-        my_new_list[idx] = element
-        return my_new_list
+
+    my_new_list = list(my_list)
+    my_new_list[idx] = element
+    return my_new_list
 
 
 if __name__ == "__main__":
