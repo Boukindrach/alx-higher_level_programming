b4f0dfd43e6f42dcd20228fc56e528d9bf28148d
Boukind Rachid <boukind.r1@gmail.com>
2023-11-04 15:26:20 -0700

    The Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for element in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][element]), end=" ")
+        print()
