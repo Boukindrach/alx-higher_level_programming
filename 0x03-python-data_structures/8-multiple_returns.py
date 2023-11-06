0b29636a4277728d1575ed15c30d449ea6170a96
Boukind Rachid <boukind.r1@gmail.com>
2023-11-06 05:22:23 -0800

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     x = len(sentence)
     if x == 0:
-        tuplr_ = x, None
+        tuple_ = x, None
     else:
         tuple_ = x, sentence[0]
     return tuple_
