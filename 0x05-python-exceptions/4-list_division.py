af0517d26b79dc786a8a16513baf56bf4f5681f8
Boukind Rachid <boukind.r1@gmail.com>
2023-11-20 18:28:09 -0800

    J

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list=[]
+    new_list = []
     for a in range(0, list_length):
         try:
             d = my_list_1[a] / my_list_2[a]
