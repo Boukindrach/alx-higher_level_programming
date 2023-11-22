a9ed7b49b0332209ed5f4799dcce387293ae3029
Boukind Rachid <boukind.r1@gmail.com>
2023-11-21 17:46:09 -0800

    B

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define a Square class"""
+"""Represent a square."""
 
 
 class Square:
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Represent a square."""
+
+
+class Square:
+    """A square."""
+
+    def __init__(self, size):
+        """Initialize a new Square with the specified size.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
