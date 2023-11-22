b4b3f7a3955157091acaaf014b84330a15e4133a
Boukind Rachid <boukind.r1@gmail.com>
2023-11-21 17:52:29 -0800

    C

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Represent a square."""
+
+
+class Square:
+    """A square."""
+
+    def __init__(self, size = 0):
+        """Initialize a new Square with the specified size.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size should be an integer")
+
+        elif size < 0:
+            raise ValueError("size should be >= 0")
+        self.__size = size
