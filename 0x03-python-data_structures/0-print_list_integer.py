db45c58f501437bad741af623b26a0dd2b685fb3
Boukind Rachid <boukind.r1@gmail.com>
2024-03-18 18:53:10 -0700

    a

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 def print_list_integer(my_list=[]):
     x = len(my_list)
     for i in range(x):
-        print("{}".format(my_list[i]))
+        numbers = "{}".format(my_list[i])
+        print(numbers)
