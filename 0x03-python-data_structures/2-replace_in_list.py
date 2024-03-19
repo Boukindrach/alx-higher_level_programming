237a8f4e0fe69a9bbc5b72a51fe55bb67f347f11
Boukind Rachid <boukind.r1@gmail.com>
2024-03-18 19:15:11 -0700

    a

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,6 @@ def replace_in_list(my_list, idx, element):
     x = len(my_list)
     if idx < 0 and idx < element:
         return None
-    for i in range(x):
-        if i == idx:
-            my_list[i] = element
-            return my_list
+    else:
+        my_list[idx] = element
+        return my_list
