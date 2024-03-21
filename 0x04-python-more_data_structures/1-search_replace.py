b2ae1e8b6801bfcaa04005dc6a1e9525b0182c23
Boukind Rachid <boukind.r1@gmail.com>
2024-03-20 17:39:44 -0700

    a

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,6 @@ def search_replace(my_list, search, replace):
     x = len(my_list)
     new_list = my_list[:]
     for i in range(x):
-        if new_list[i] == 2:
-            new_list[i] = 89
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
