b733273ce012cff8521e089ad81523b58faf83ba
Boukind Rachid <boukind.r1@gmail.com>
2024-04-11 02:53:24 +0000

    a

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,71 +3,77 @@
 import sys
 
 
-def initialize_board(n):
-	"""Initialize an n x n chessboard with empty cells."""
-	board = [[' ' for _ in range(n)] for _ in range(n)]
-	return board
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
+
 
 def mark_attack_spots(board, row, col):
-	"""Mark the spots attacked by a queen."""
 	n = len(board)
 	for i in range(n):
-		board[row][i] = 'x'  # Mark horizontally
-		board[i][col] = 'x'  # Mark vertically
+		board[row][i] = 'x'
+		board[i][col] = 'x'
 
-	if 0 <= row + i < n and 0 <= col + i < n:
-		board[row + i][col + i] = 'x'  # Mark diagonally down-right
+	if row - i >= 0 and col + i < n:
+		board[row - i][col + i] = "x"
+	if row + i < n and col + i < n:
+		board[row + i][col + i] = "x"
+	if row - i >= 0 and col - i >= 0:
+		board[row - i][col - i] = "x"
+	if row + i < n and col - i >= 0:
+		board[row + i][col - i] = "x"
 
-	if 0 <= row - i < n and 0 <= col - i < n:
-		board[row - i][col - i] = 'x'  # Mark diagonally up-left
-
-	if 0 <= row + i < n and 0 <= col - i < n:
-		board[row + i][col - i] = 'x'  # Mark diagonally down-left
-
-	if 0 <= row - i < n and 0 <= col + i < n:
-		board[row - i][col + i] = 'x'  # Mark diagonally up-right
 
 
 def recursive_solve(board, row, queens, solutions):
-	"""Recursively solve the N-queens puzzle."""
-	n = len(board)
-	if queens == n:
-		solutions.append([row[:] for row in board])
-		return solutions
+    if queens == len(board):
+        solutions.append(get_solution(board))
+        return (solutions)
 
-	for col in range(n):
-		if board[row][col] == ' ':
-			mark_attack_spots(board, row, col)
-			board[row][col] = 'Q'
-			solutions = recursive_solve(board, row + 1, queens + 1, solutions)
-			mark_attack_spots(board, row, col)
-			board[row][col] = ' '
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_deepcopy(board)
+            tmp_board[row][c] = "Q"
+            mark_attack_spots(tmp_board, row, c)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
 
-	return solutions
+    return (solutions)
 
-def print_solution(solution):
-	"""Print the solution in a readable format."""
-	formatted_solution = []
-	for pos in solution:
-		formatted_solution.append([pos[0], pos[1]])
-	print(formatted_solution)
 
 if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		print("Usage: nqueens N")
-		sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-	try:
-		n = int(sys.argv[1])
-	except ValueError:
-		print("N must be a number")
-		sys.exit(1)
-
-	if n < 4:
-		print("N must be a number greater than or equal to 4")
-		sys.exit(1)
-
-	board = initialize_board(n)
-	solutions = recursive_solve(board, 0, 0, [])
-	for solution in solutions:
-		print_solution(solution)
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
