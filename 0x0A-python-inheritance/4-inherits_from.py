0a8bf327d72210f572c04bcf3f7a243f41eaf7b5
Boukind Rachid <boukind.r1@gmail.com>
2024-04-15 20:46:15 +0000

    a

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,6 +14,6 @@ def inherits_from(obj, a_class):
 	Returns:
 		True if obj is an inherited instance of a_class, otherwise False.
 	"""
-	if issubclass(type(obj), a_class) and type(obj) != a_class
+	if issubclass(type(obj), a_class) and type(obj) != a_class:
 		return True
 	return False
