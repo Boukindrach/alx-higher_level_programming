6ee2613ce689461e7c2c0332b1f2ae69da8c6ba7
Boukind Rachid <boukind.r1@gmail.com>
2024-04-15 20:50:46 +0000

    a

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,6 +14,4 @@ def inherits_from(obj, a_class):
 	Returns:
 		True if obj is an inherited instance of a_class, otherwise False.
 	"""
-	if issubclass(type(obj), a_class) and type(obj) != a_class:
-		return True
-	return False
+	return issubclass(type(obj), a_class) and type(obj) != a_class
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines a BaseGeometry class for geometry calculations."""
+
+
+class BaseGeometry:
+	"""Base class for geometric shapes."""
+
+	def area(self):
+		"""Raises an exception for unimplemented area calculation."""
+		raise Exception("area() is not implemented")
