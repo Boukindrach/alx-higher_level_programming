66d8109f3cbd2b659a3a1267f042efa520de676f
Boukind Rachid <boukind.r1@gmail.com>
2024-04-15 20:41:05 +0000

    a

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a function to check inheritance of a class."""
+
+
+def inherits_from(obj, a_class):
+
+	"""Verify if an object is an inherited instance of a specific class.
+
+	Args:
+		obj (any): The object to check.
+		a_class (type): The class to compare against the type of obj.
+
+	Returns:
+		True if obj is an inherited instance of a_class, otherwise False.
+	"""
+	return issubclass(type(obj), a_class) and type(obj) != a_class
