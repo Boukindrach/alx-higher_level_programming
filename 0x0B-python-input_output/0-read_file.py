e8543955a6985665086961ada5795e7f6625cda8
Boukind Rachid <boukind.r1@gmail.com>
2024-04-17 01:40:47 +0000

    a

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,11 +3,6 @@
 
 
 def read_file(filename=""):
-    """Prints UTF-8 encoded text file to the standard output.
-
-    Args:
-        filename (str, optional): The name of the file to read.
-	Defaults to an empty string.
-    """
+    """Prints UTF-8 encoded text file to the standard output."""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
