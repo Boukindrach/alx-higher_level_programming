736e35021d76df38cf2f1ebe1ecef31ed94b065a
Boukind Rachid <boukind.r1@gmail.com>
2024-04-22 12:35:38 -0700

    a

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-"""Rectangle module"""
+"""Defines a rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
     """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
