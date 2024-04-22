3640c91759a8644d95c67f443e8054a783dbc67b
Boukind Rachid <boukind.r1@gmail.com>
2024-04-22 07:42:30 -0700

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 
 class Base:
     __nb_objects = 0
@@ -10,3 +11,22 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries or list_dictionaries is not None:
+            return json.dumps(list_dictionaries)
+        else:
+            return ("[]")
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -2,6 +2,9 @@
 
 import unittest
 from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
+import os
 
 
 class TestBase(unittest.TestCase):
@@ -23,6 +26,58 @@ class TestBase(unittest.TestCase):
         obj3 = Base()
         self.assertEqual(Base._Base__nb_objects, 3)
 
+class Test_JsonString_Function_Rectangle(unittest.TestCase):
+
+    def test_to_json_string_with_list(self):
+        list_dicts = [{'id': 1, 'height': 20}, {'id': 2, 'width': 10, 'x': 0, 'y': 6}]
+        expected_json_str = '[{"id": 1, "height": 20}, {"id": 2, "width": 10, "x": 0, "y": 6}]'
+        self.assertEqual(Rectangle.to_json_string(list_dicts), expected_json_str)
+
+    def test_to_json_string_with_empty_list(self):
+        self.assertEqual(Rectangle.to_json_string([]), "[]")
+
+    def test_to_json_string_with_none(self):
+        self.assertEqual(Rectangle.to_json_string(None), "[]")
+
+class Test_JsonString_Function_Square(unittest.TestCase):
+
+    def test_to_json_string_with_list(self):
+        list_dicts = [{'id': 1, 'size': 15}, {'id': 2, 'size': 20, 'x': 1}]
+        expected_json_str = '[{"id": 1, "size": 15}, {"id": 2, "size": 20, "x": 1}]'
+        self.assertEqual(Square.to_json_string(list_dicts), expected_json_str)
+
+    def test_to_json_string_with_empty_list(self):
+        self.assertEqual(Square.to_json_string([]), "[]")
+
+    def test_to_json_string_with_none(self):
+        self.assertEqual(Square.to_json_string(None), "[]")
+
+class TestSaveToFileFunction(unittest.TestCase):
+
+    class TestClass:
+        def __init__(self, id=None, width=None):
+            self.id = id
+            self.width = width
+
+        def to_dictionary(self):
+            return {'id': self.id, 'width': self.width}
+
+    def test_save_to_file_with_objects(self):
+        obj1 = self.TestClass(id=1, width=10)
+        obj2 = self.TestClass(id=2, width=15)
+        Square.save_to_file([obj1, obj2])
+        
+        with open("TestClass.json", "w") as file:
+            content = file.read()
+            expected_content = '[{"id": 1, "width": 10}]', '[{"id": 2, "width": 15}]'
+            self.assertEqual(content, expected_content)
+
+    def test_save_to_file_with_empty_list(self):
+        Square.save_to_file([])
+        
+        with open("TestClass.json", "w") as file:
+            content = file.read()
+            self.assertEqual(content, "[]")
 
 if __name__ == '__main__':
     unittest.main()
