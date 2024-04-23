c6460106076be5cd2d6ad7548149fe4bdc323c5c
Boukind Rachid <boukind.r1@gmail.com>
2024-04-22 18:50:40 -0700

    a

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,56 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
+
+   @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Get/set the size of the Square."""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the Square."""
+        if args:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    self.id = arg
+                elif count == 1:
+                    self.size = arg
+                elif count == 2:
+                    self.x = arg
+                elif count == 3:
+                    self.y = 3
+                count += 1
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {
+                'id': self.id,
+                'x': self.x,
+                'size': self.size,
+                'y': self.y
+                }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id,
+                self.x,
+                self.y,
+                self.size) 	
