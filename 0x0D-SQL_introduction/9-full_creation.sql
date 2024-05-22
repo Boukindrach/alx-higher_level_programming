410e39c0c3ac5238eb57c2adbcf4aabb63871f1b
Boukind Rachid <boukind.r1@gmail.com>
2024-05-21 18:19:39 -0700

    a

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- script that creates a table second_table in the database hbtn_0c_0
+
 CREATE TABLE IF NOT EXISTS `second_table` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(256) NOT NULL,
   `score` INT NOT NULL,
   PRIMARY KEY (`id`)
 );
-
 INSERT INTO `second_table` (`name`, `score`) VALUES
   ("John", 10),
   ("Alex", 3),
