a7e206aeda14ca3892ca843963084d47d37d5582
Boukind Rachid <boukind.r1@gmail.com>
2024-05-22 19:20:29 -0700

    a

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT s.title, GROUP_CONCAT(g.genre_id) AS genres
-FROM tv_shows AS s
-	LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
-	GROUP BY s.id, s.title
-ORDER BY s.title;
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
