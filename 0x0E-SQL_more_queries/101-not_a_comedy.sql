114f54e5f61fba872349a675d1b0cb2243a74d1d
Boukind Rachid <boukind.r1@gmail.com>
2024-05-22 19:25:07 -0700

    a

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+--
+SELECT DISTINCT t.`title`
+FROM `tv_shows` AS t
+LEFT JOIN `tv_show_genres` AS s ON s.`show_id` = t.`id`
+LEFT JOIN `tv_genres` AS g ON g.`id` = s.`genre_id`
+WHERE t.`title` NOT IN (
+    SELECT DISTINCT t2.`title`
+    FROM `tv_shows` AS t2
+    INNER JOIN `tv_show_genres` AS s2 ON s2.`show_id` = t2.`id`
+    INNER JOIN `tv_genres` AS g2 ON g2.`id` = s2.`genre_id`
+    WHERE g2.`name` = "Comedy"
+)
+ORDER BY t.`title`;
