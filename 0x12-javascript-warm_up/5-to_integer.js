44178a7f0cc295b17e7a00c8545e9c3d81e7a26a
Boukind Rachid <boukind.r1@gmail.com>
2024-06-18 09:45:38 -0700

    a

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ if (process.argv.length === 2) {
   console.log('Not a number');
 } else if (process.argv.length === 3) {
   if (!isNaN(parseFloat(process.argv[2])) && isFinite(process.argv[2])) {
-    console.log(process.argv[2]);
+    console.log('My number:', process.argv[2]);
   } else {
     console.log('Not a number');
   }
