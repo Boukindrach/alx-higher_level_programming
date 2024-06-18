8f6a0f9ca71b0d6cd4f23dffe3f9b2451cefe837
Boukind Rachid <boukind.r1@gmail.com>
2024-06-18 11:18:47 -0700

    finally

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
-if (process.argv.length <= 3){
+if (process.argv.length <= 3) {
   console.log('NaN');
 } else {
-  let sum = 0;
-  for (let i = 2; i < process.argv.length; i++) {
-    sum += Number(process.argv[i]);
-  }
-  console.log(sum)
+  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
 }
