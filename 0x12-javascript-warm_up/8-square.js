be2dfcb3ee960c542ec782946c14570379468988
Boukind Rachid <boukind.r1@gmail.com>
2024-06-18 10:50:25 -0700

    a

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-    if (!isNaN(parseFloat(process.argv[2])) && isFinite(process.argv[2])) {
-	    for (let i = 0; i < process.argv[2]; i++) {
-		    box = ''
-		    for (let j = 0; j < process.argv[2]; j++) box += 'X';{
-			    console.log(box);
-		    }
-	    }
-     } else {
-         console.log('Missing size');
+  if (!isNaN(parseFloat(process.argv[2])) && isFinite(process.argv[2])) {
+    for (let i = 0; i < process.argv[2]; i++) {
+      let box = '';
+      for (let j = 0; j < process.argv[2]; j++) box += 'X';
+      console.log(box);
+    }
+  } else {
+    console.log('Missing size');
   }
 }
