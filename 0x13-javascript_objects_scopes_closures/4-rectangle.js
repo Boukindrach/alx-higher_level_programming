684e5e97bfdf5d78e3d54315d5b9c7294fd00006
Boukind Rachid <boukind.r1@gmail.com>
2024-06-19 03:39:49 -0700

    a

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let box = '';
+      for (let j = 0; j < this.width; j++) box += 'X';
+      console.log(box);
+    }
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+
+  rotate () {
+    const tmp;
+    tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+};
