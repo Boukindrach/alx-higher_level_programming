a819205cb39c5cd5c34f31bd11ef75887ab60c87
Boukind Rachid <boukind.r1@gmail.com>
2024-06-28 17:42:48 -0700

    a

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding City objects contained in the database.
+Lists all State objects and corresponding
+City objects contained in the database.
 """
 import sys
 from relationship_state import Base, State
@@ -13,14 +14,15 @@ if __name__ == '__main__':
     db_password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{db_user}:{db_password}@localhost:3306/{db_name}',
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(db_user, db_password, db_name),
                            pool_pre_ping=True)
-    
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
-    for state in states:
+    s = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    for state in s:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
             print(f"    {city.id}: {city.name}")
