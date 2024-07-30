ae8f1c884412e1c9af62acfbd5de45c72b00e630
Boukind Rachid <boukind.r1@gmail.com>
2024-07-30 12:44:04 -0700

    a

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,8 +6,6 @@ const apiUrl = process.argv[2];
 request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
   } else {
       const films = JSON.parse(body).results;
       const wedgeFilms = films.filter(film => 
