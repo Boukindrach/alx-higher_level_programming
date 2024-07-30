d1c8dbee2b5f4ddf5f7efb0e379f539f7cfd9d7c
Boukind Rachid <boukind.r1@gmail.com>
2024-07-30 12:48:28 -0700

    a

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,14 +8,10 @@ const filePath = process.argv[3];
 request.get(url, (error, response, body) => {
   if (error) {
     console.error('Error making request:', error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: Status code ${response.statusCode}`);
   } else {
     fs.writeFile(filePath, body, 'utf-8', (writeError) => {
       if (writeError) {
         console.error('Error writing to file:', writeError);
-      } else {
-        console.log(`Webpage content has been saved to ${filePath}`);
       }
     });
   }
